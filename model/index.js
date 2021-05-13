8827067aff241933553f96cf9f59e526bc5fcd21
Yevheniia Suska <susskaya86@gmail.com>
2021-05-13 04:28:37 +0300

    реализованы получение списка контактов и создание нового контакта

--- model/index.js
+++ model/index.js
@@ -1,14 +1,59 @@
-// const fs = require('fs/promises')
+const path = require('path')
+const fs = require('fs/promises')
+const { v4: uuid } = require('uuid')
 // const contacts = require('./contacts.json')
 
-const listContacts = async () => {}
+const contactsPath = path.join(__dirname, 'contacts.json')
 
+// список всех контактов
+const listContacts = async () => {
+  try {
+    const data = await fs.readFile(contactsPath)
+    return (JSON.parse(data))
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+//
 const getContactById = async (contactId) => {}
 
+// удалить контакт
 const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+// создать новый  контакт
+const addContact = async({ name, email, phone, englishSpeaking }) => {
+  try {
+    const body = { name, email, phone, englishSpeaking }
+    if (!name || !email || !phone) {
+      return
+    }
+    const data = await fs.readFile(contactsPath)
+    const id = uuid()
+    const contacts = JSON.parse(data)
 
+    const newContact = {
+      id,
+      ...body,
+      ...(englishSpeaking ? {} : { englishSpeaking: false })
+    }
+    const newContactsList = JSON.stringify([...contacts, newContact], null, 2)
+    await fs.writeFile(contactsPath, newContactsList)
+    // if (!body.name || !body.email || !body.phone) {
+    //   return contacts
+    // }
+
+    return newContact
+
+    // const record = { id, ...body }
+    // data.push(record)
+    // return (JSON.parse(record))
+  } catch (err) {
+    console.log(err.message)
+  }
+}
+
+// редактировать контакт
 const updateContact = async (contactId, body) => {}
 
 module.exports = {
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,45 @@
 const express = require('express')
 const router = express.Router()
+const Contacts = require('../../model/index')
+// const { listContacts } = require('../../model/index')
 
+// список всех контактов
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const contacts = await Contacts.listContacts()
+    // если после res не писать status(), он по умолчанию 200
+    return res.json({ status: 'success', code: 200, data: { contacts } })
+  } catch (err) {
+    next(err)
+  }
 })
 
+//
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template2 message' })
 })
 
+// создать контакт
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { name, email, phone, englishSpeaking } = req.body
+    const contact = await Contacts.addContact({ name, email, phone, englishSpeaking })
+    if (!name || !email || !phone) {
+      return res.status(400).json({ status: 'bad request', code: 400, message: 'missing required name field' })
+    } return res.status(201).json({ status: 'success', code: 201, data: { contact } })
+  } catch (err) {
+    next(err)
+  }
 })
 
+// удалить контакт
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template4 message' })
 })
 
+//
 router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  res.json({ message: 'template5 message' })
 })
 
 module.exports = router
